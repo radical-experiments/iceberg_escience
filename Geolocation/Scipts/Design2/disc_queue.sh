f5cb87b7f981fccd51f9e95e5a41ab1fdbbed2c0
AymenFJA <aymenfadhel@yahoo.com>
2020-01-31 12:26:11 -0500

    moving files

--- Geolocation/Scipts/Design2/disc_queue.sh
+++ Geolocation/Scipts/Design2/disc_queue.sh
@@ -5,6 +5,7 @@ module load intel/19.5
 module load xdusage/2.1-1
 module load anaconda2
 
+
 source activate /pylon5/mc3bggp/aymen/anaconda3/envs/geo
 
 python q1.py --queue /pylon5/mc3bggp/aymen/Des3Test/discovered > discovery_queue.log
