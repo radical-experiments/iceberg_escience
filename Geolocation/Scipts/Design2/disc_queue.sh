b29b2baf951be7db96d92439236c23f1f5bc90dc
AymenFJA <aymenfadhel@yahoo.com>
2020-01-30 23:14:56 -0600

    switching to conda_env

--- Geolocation/Scipts/Design2/disc_queue.sh
+++ Geolocation/Scipts/Design2/disc_queue.sh
@@ -3,9 +3,8 @@ module load psc_path/1.1
 module load slurm/default
 module load intel/19.5
 module load xdusage/2.1-1
+module load anaconda2
 
-
-source /pylon5/mc3bggp/aymen/penguins_pkg/bin/activate
-export PYTHONPATH=/pylon5/mc3bggp/aymen/penguins_pkg/lib/python2.7/site-packages:$PYTHONPATH
+source activate /pylon5/mc3bggp/aymen/anaconda3/envs/geo
 
 python q1.py --queue /pylon5/mc3bggp/aymen/Des3Test/discovered > discovery_queue.log
