f1dcd5d3c70e15a68b9d3991c57b0aa062c88cb5
Aymen <aymenfadhel@yahoo.com>
2020-02-06 13:09:56 -0500

    fiixng queue paths

--- Geolocation/Scipts/Design2/firstnode.sh
+++ Geolocation/Scipts/Design2/firstnode.sh
@@ -9,16 +9,16 @@ module load anaconda2
 source activate /pylon5/mc3bggp/aymen/anaconda3/envs/geo
 
 
-python q1.py --queue geolocate > geolocate_queue.log &
+python q1.py --queue /pylon5/mc3bggp/aymen/Des3Test/geolocate > geolocate_queue.log &
 
 sleep 1
 
-CUDA_VISIBLE_DEVICES=0 python geolocate.py --name geolocate1 --queue_in /pylon5/mc3bggp/aymen/Des3Test/discovered.queue.url  --queue_out geolocate.queue.url >geo1.log &
-CUDA_VISIBLE_DEVICES=1 python geolocate.py --name geolocate2 --queue_in /pylon5/mc3bggp/aymen/Des3Test/discovered.queue.url  --queue_out geolocate.queue.url >geo2.log &
+CUDA_VISIBLE_DEVICES=0 python geolocate.py --name geolocate1 --queue_in /pylon5/mc3bggp/aymen/Des3Test/discovered.queue.url  --queue_out /pylon5/mc3bggp/aymen/Des3Test/geolocate.queue.url >geo1.log &
+CUDA_VISIBLE_DEVICES=1 python geolocate.py --name geolocate2 --queue_in /pylon5/mc3bggp/aymen/Des3Test/discovered.queue.url  --queue_out /pylon5/mc3bggp/aymen/Des3Test/geolocate.queue.url >geo2.log &
 
 sleep 1
 
-python ransac.py --name ransac1 --queue_in geolocate.queue.url >ransac1.log &
+python ransac.py --name ransac1 --queue_in /pylon5/mc3bggp/aymen/Des3Test/geolocate.queue.url >ransac1.log &
 
 wait
 
--- Geolocation/Scipts/Design2/secondnode.sh
+++ Geolocation/Scipts/Design2/secondnode.sh
@@ -9,16 +9,16 @@ module load anaconda2
 source activate /pylon5/mc3bggp/aymen/anaconda3/envs/geo
 
 
-python q1.py --queue geolocate > geolocate_queue.log &
+python q1.py --queue /pylon5/mc3bggp/aymen/Des3Test/geolocate > geolocate_queue.log &
 
 sleep 1
 
-CUDA_VISIBLE_DEVICES=0 python geolocate.py  --name geolocate3 --queue_in /pylon5/mc3bggp/aymen/Des3Test/discovered.queue.url  --queue_out geolocate.queue.url >geo3.log &
-CUDA_VISIBLE_DEVICES=1 python geolocate.py  --name geolocate4 --queue_in /pylon5/mc3bggp/aymen/Des3Test/discovered.queue.url  --queue_out geolocate.queue.url >geo4.log &
+CUDA_VISIBLE_DEVICES=0 python geolocate.py  --name geolocate3 --queue_in /pylon5/mc3bggp/aymen/Des3Test/discovered.queue.url  --queue_out /pylon5/mc3bggp/aymen/Des3Test/geolocate.queue.url >geo3.log &
+CUDA_VISIBLE_DEVICES=1 python geolocate.py  --name geolocate4 --queue_in /pylon5/mc3bggp/aymen/Des3Test/discovered.queue.url  --queue_out /pylon5/mc3bggp/aymen/Des3Test/geolocate.queue.url >geo4.log &
 
 sleep 1 
 
-python ransac.py --name ransac2 --queue_in geolocate.queue.url > ransac2.log &
+python ransac.py --name ransac2 --queue_in /pylon5/mc3bggp/aymen/Des3Test/geolocate.queue.url > ransac2.log &
 
 wait
 
