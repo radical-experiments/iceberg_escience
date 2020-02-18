9fd145ed216d0a073d0b1fd39adb17d77a5283e2
AymenFJA <aymenfadhel@yahoo.com>
2020-02-18 12:23:52 -0600

    adding Design2a node images  file

--- Geolocation/Scipts/Design2a/image_dists.py
+++ Geolocation/Scipts/Design2a/image_dists.py
@@ -9,14 +9,15 @@ if __name__ == "__main__":
 
     node_dists = list()
     for i in range(nodes):
-        df = pd.DataFrame(columns=['Filename','Size'])
+        df = pd.DataFrame(columns=['ImageName1','ImageName2','SIZE1','SIZE2','X1','Y1','X2','Y2'])
         node_dists.append(df)
 
-    images.sort_values('Size', axis=0,inplace=True)
+    images.sort_values(['SIZE1']+['SIZE2'])
     images.reset_index(drop='index',inplace=True)
+
     for idx, row in images.iterrows():
         node_idx = idx % nodes
-        node_dists[node_idx].loc[len(node_dists[node_idx])] = [row['Filename'],row['Size']]
+        node_dists[node_idx].loc[len(node_dists[node_idx])] = [row['ImageName1'],row['ImageName2'],row['SIZE1'],row['SIZE2'],row['X1'],row['Y1'],row['X2'],row['Y2']]
 
     for i in range(nodes):
         node_dists[node_idx].to_csv('node%i_images.csv' % i,index=False)
